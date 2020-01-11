8d45abd513750812c42692c97ba41aa5612aeea4
emathur <emathur@cisco.com>
2020-01-11 14:55:58 +0530

    add screenshot

---
+++ src/main/java/com/esh/recursion/Smallest.java
@@ -0,0 +1,15 @@
+package com.esh.recursion;
+
+public class Smallest {
+
+	public static void main(String[] args) {
+		int[] nums = {2,1,3,47,10};
+		System.out.println("The minimum number is: " + min(nums));
+	}
+
+	private static String min(int[] nums) {
+		int minIndex = 0;
+		
+		return null;
+	}
+}
